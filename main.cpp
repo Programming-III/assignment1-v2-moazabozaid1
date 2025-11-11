9a4eaeec9f4ac61bb712e8d56acdc681956cec68
moazabozaid1 <moaz.abozaid1@gmail.com>
2025-11-11 09:55:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,44 +5,68 @@
 #include "Instructor.h"
 #include "Course.h"
 
-
 using namespace std;
 
-// ==================== Person Class Implementation =========================
+Person::Person() {
+	name = "null";
+	id = 0;
+}
 
+Person::Person(string n, int i) {
+	name = n;
+	id = i;
+}
 
+void Person::displayPersonInfo() {
+	cout << "name:" << name << endl;
+	cout << "ID:" << id << endl;
+}
 
+Student::Student() {
+	yearLevel = 0;
+	major = "null";
+}
 
+Student::Student(int y, string m) {
+	yearLevel = y;
+	major = "null";
+}
+void Student:: displayStudentInfo() {
+	cout << "yearlevel:" << yearLevel << endl;
+	cout << "major:" << major << endl;
+}
+Instructor::Instructor() {
+	department = "null";
+	experienceYears = 0;
+}
 
+Instructor::Instructor(string d, int e) {
+	department = d;
+	experienceYears = e;
+}
+void Instructor::displayInstructorInfo() {
+	cout << "Department:" << department << endl;
+	cout << "ExpYears:" << experienceYears << endl;
+}
 
+course::course() {
+	courseCode = "null";
+	courseName = "null";
+	maxStudents = 0;
 
+}
+course::course(string code, string name, int max, int current) {
+	courseCode = code;
+	courseName = name;
+	maxStudents = max;
+	currentStudents = current;
+}
+void course::displayCourseInfo() {
+	cout << courseCode << endl;
+	cout << courseName << endl;
+	cout << maxStudents << endl;
+	cout << currentStudents << endl;
 
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
-int main() {
-   
-    
-    return 0;
+}
+return 0;
 }
